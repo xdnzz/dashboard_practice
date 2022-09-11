ebf2689f863408e773c6be59cb13036efb66186c
Danilo <danilosdinizz@gmail.com>
2022-09-11 16:27:42 -0300

    feat: sidebar adicionada

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,15 @@
+import { Flex } from "@chakra-ui/react";
+import { Header } from "../components/Header";
+import { Sidebar } from "./Sidebar";
+
+
+export default function Dashboard() {
+    return (
+        <Flex direction="column" h="100vw">
+            <Header />
+            <Flex w="100%" my="6" maxWidth={1460} mx="auto" px="6">
+                <Sidebar />
+            </Flex>
+        </Flex>
+    )
+}
