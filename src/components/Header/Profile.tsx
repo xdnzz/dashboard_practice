d7b02026b2cd86b1850c3911de487a8b4e062087
Danilo <danilosdinizz@gmail.com>
2022-10-06 16:23:56 -0300

    chore: responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,22 @@
-import { Flex, Box, Text, Avatar} from "@chakra-ui/react";
+import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface profileProps {
+    showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: profileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="center">
-                <Text>
-                    Dio Brando
-                </Text>
-                <Text color="gray.300" fontSize="small">
-                    diobrando@imortal.shell
-                </Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="center">
+                    <Text>
+                        Dio Brando
+                    </Text>
+                    <Text color="gray.300" fontSize="small">
+                        diobrando@imortal.shell
+                    </Text>
+                </Box>
+            )}
             <Avatar size="md" name="Dio Brando" src="https://github.com/xdnzz.png" />
         </Flex>
     )
