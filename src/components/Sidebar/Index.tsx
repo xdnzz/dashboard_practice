890957c1c38e05f3ed26e4721216363c770d3b35
Danilo <danilosdinizz@gmail.com>
2022-10-06 19:06:08 -0300

    chore: adding responsivity in the sidebar

--- src/components/Sidebar/Index.tsx
+++ src/components/Sidebar/Index.tsx
@@ -1,24 +1,28 @@
-import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
-import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
-import { NavLink } from "./NavLink";
-import { NavSection } from "./NavSection";
+import { Box, Drawer, DrawerCloseButton, DrawerContent, DrawerOverlay, useBreakpointValue} from "@chakra-ui/react";
+import { SidebarNav } from "./SidbarNav";
 
 
 
 
 export function Sidebar() {
+
+    const isFloatingSidebar = useBreakpointValue({
+        base: true,
+        lg: false
+    });
+
+    if(isFloatingSidebar){
+        <Drawer isOpen={true} placement="left" onClose={()=>{}}>
+            <DrawerOverlay>
+                <DrawerContent>
+                    <DrawerCloseButton mt="6"/>
+                </DrawerContent>
+            </DrawerOverlay>
+        </Drawer>
+    }
     return (
         <Box as="aside" w="64" mr="8">
-            <Stack spacing="12" align="flex-start">
-                <NavSection title="GERAL">
-                    <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-                    <NavLink icon={RiContactsLine}>Usuários</NavLink>
-                </NavSection>
-                <NavSection title="Automação">
-                    <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
-                    <NavLink icon={RiGitMergeLine}>Automação</NavLink>
-                </NavSection>
-            </Stack>
+           <SidebarNav/>
         </Box>
     )
 }
